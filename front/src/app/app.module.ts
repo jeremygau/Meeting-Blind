c9cdb0aca9e8ee37c00c772a4a000c99f6f72667
robin <robinseverac@hotmail.fr>
2021-02-18 11:09:41 +0100

    HttpService created

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateSeekerComponent } from './date-seeker/date-seeker.component';
+import { DateListComponent } from './date-list/date-list.component';
+import { HttpService } from './services/http.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateSeekerComponent,
+    DateListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
