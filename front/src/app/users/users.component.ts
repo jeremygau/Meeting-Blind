617edb737894d531eedd5c0fb827f20f7e39d99c
robin <robinseverac@hotmail.fr>
2021-02-25 13:23:11 +0100

    app users module done

---
+++ front/src/app/users/users.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  @Input() firstName!: string;
+  @Input() id!: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
